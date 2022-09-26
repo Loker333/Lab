326ea9727d54a590a8c174d0f599ea0826ae9a68
loker.statist.2003@mail.ru <loker.statist.2003@mail.ru>
2022-09-26 14:38:34 +0300

    готово

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -10,6 +11,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Extensions;
+using NLog;
+using System.IO;
 
 namespace WebApplication1
 {
@@ -17,6 +21,8 @@ namespace WebApplication1
     {
         public Startup(IConfiguration configuration)
         {
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+            "/nlog.config"));
             Configuration = configuration;
         }
 
@@ -25,7 +31,11 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
+            services.ConfigureLoggerService();
             services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +47,13 @@ namespace WebApplication1
             }
 
             app.UseHttpsRedirection();
+            app.UseHsts();
+            app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                ForwardedHeaders = ForwardedHeaders.All
+                });
 
             app.UseRouting();
 
