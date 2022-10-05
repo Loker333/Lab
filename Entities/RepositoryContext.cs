6f30effd8dafd350951e1349f0dc99277a92758e
loker <Nik.borodulin.2003@mail.ru>
2022-10-05 11:10:06 +0300

    Готово

---
+++ Entities/RepositoryContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
