8d091754cd828be965d0554a6abd86e1a4eba84f
loker <Nik.borodulin.2003@mail.ru>
2022-10-19 11:06:33 +0300

    готово, а нет

--- WebApplication1/Extensions/ServiceExtensions.cs
+++ WebApplication1/Extensions/ServiceExtensions.cs
@@ -26,6 +26,11 @@ namespace WebApplication1.Extensions
         });
         public static void ConfigureLoggerService(this IServiceCollection services) =>
         services.AddScoped<ILoggerManager, LoggerManager>();
-        
+
+        public static void ConfigureSqlContext(this IServiceCollection services,
+        IConfiguration configuration) =>
+        services.AddDbContext<RepositoryContext>(opts =>
+        opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b =>
+        b.MigrationsAssembly("WebApplication1")));
     }
 }
