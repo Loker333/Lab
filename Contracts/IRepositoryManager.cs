b1ad5e40de5df3d3182672d7835c4e8b2feb641e
loker.statist.2003@mail.ru <loker.statist.2003@mail.ru>
2022-11-04 18:54:59 +0300

    Выполнил

--- Contracts/IRepositoryManager.cs
+++ Contracts/IRepositoryManager.cs
@@ -9,6 +9,7 @@ namespace Contracts
         ICompanyRepository Company { get; }
         IEmployeeRepository Employee { get; }
         IPizzeriaRepository Pizzeria { get; }
+        IMenuRepository Menu { get; }
         void Save();
     }
 }
